08184751ff3f774ea7bf10671bbf588752a019c2
Arda Özcan <ardaozcan05@gmail.com>
2025-08-15 17:20:10 +0300

    Fixed main metadata

--- main.c
+++ main.c
@@ -6,19 +6,19 @@
 int
 main(void)
 {
-    GIFMetadata metadata = (GIFMetadata){ .version = GIF87a,
-                                          .background = 0x0f,
-                                          .color_resolution = 6,
+    GIFMetadata metadata = (GIFMetadata){ .version = GIF89a,
+                                          .background = 0x10,
+                                          .color_resolution = 2,
                                           .sort = 0,
                                           .local_color_table = 0,
                                           .pixel_aspect_ratio = 0,
-                                          .min_code_size = 4,
-                                          .gct_size_n = 3,
+                                          .min_code_size = 6,
+                                          .gct_size_n = 5,
                                           .left = 0,
                                           .top = 0,
-                                          .width = 256,
-                                          .height = 256,
-                                          .image_extension = false,
+                                          .width = 64,
+                                          .height = 64,
+                                          .image_extension = true,
                                           .has_gct = true };
 
     gif_export(metadata, cat64_colors, cat64_indices, "out/out.gif");
